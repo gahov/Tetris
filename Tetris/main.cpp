65d4ea490f5b78ecf9c3daaa8557d232735b2a97
slawa <slawa>
2016-12-10 13:30:20 +0200

    Initial commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,10 @@
+#include "tetris.hpp"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Tetris w;
+	w.show();
+	return a.exec();
+}
---
+++ Tetris/tetris.cpp
@@ -0,0 +1,12 @@
+#include "tetris.hpp"
+
+Tetris::Tetris(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+Tetris::~Tetris()
+{
+
+}
